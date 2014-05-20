37b46af2e79ded8dd19b193429df29538e4afdbb
Gavin Li <acabin@live.com>
2014-05-20 22:27:33 +0800

    is_telephone

--- kerriframe/functions.php
+++ kerriframe/functions.php
@@ -1,11 +1,12 @@
 <?php
-
 function is_email($email) {
-	if(defined('FILTER_VALIDATE_EMAIL')) {
+	if (defined('FILTER_VALIDATE_EMAIL')) {
 		return filter_var($email, FILTER_VALIDATE_EMAIL);
 	} else {
 		return (bool)preg_match('#^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$#', $email);
 	}
-
 }
 
+function is_telephone($telephone) {
+	return (bool)preg_match('#^[0-9]{11}$#', $telephone);
+}
